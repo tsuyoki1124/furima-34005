121671de4649379b9a6147fed2b9592f2b16beb7
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-11 17:30:05 +0900

    faker導入

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'aaa000'}
+    nickname              {Faker::Name.initials(number: 2)}
+    email                 {Faker::Internet.free_email}
+    password              {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
     last_name             {'田中'}
     first_name            {'太郎'}
