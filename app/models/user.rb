f96a26e94ce407ac5f2f6348cb7c97f0f19f4a1d
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-11 12:46:13 +0900

    validation 重複解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  validates_format_of :password, with: PASSWORD_REGEX, allow_blank: true
   validates :nickname, presence: true
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, allow_blank: true } do
     validates :last_name
     validates :first_name
   end
-  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: '全角文字を使用してください' } do
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, allow_blank: true } do
     validates :last_name_kana
     validates :first_name_kana
   end
