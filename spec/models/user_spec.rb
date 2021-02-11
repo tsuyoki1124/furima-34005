c275a5bd00c88a3595b9521751a2acc2593bde69
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-11 19:47:02 +0900

    名前のテストコード

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,8 +6,11 @@ RSpec.describe User, type: :model do
   end
 
   describe 'ユーザー新規登録' do
+    it '全てが正しい値が入力されていれば登録できる' do
+      expect(@user).to be_valid
+    end
 
-    #空では登録できないテスト
+    #空では登録できないテスト,null: false
     it 'nicknameが空では新規登録できない' do
       @user.nickname = ''
       @user.valid?
@@ -112,7 +115,57 @@ RSpec.describe User, type: :model do
     end
 
     #名前のテスト
-    
+    it 'last_nameは半角だと登録できない' do
+      @user.last_name = 'ｱｱｱｱ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name is invalid")
+    end
+
+    it 'last_nameは数字では登録できない' do
+      @user.last_name = '１１１'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name is invalid")
+    end
+
+
+    it 'first_nameは半角だと登録できない' do
+      @user.first_name = 'ｱｱｱｱ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name is invalid")
+    end
+
+    it 'first_nameは数字では登録できない' do
+      @user.first_name = '１１１'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name is invalid")
+    end
+
+    it 'last_name_kanaは半角だと登録できない' do
+      @user.last_name_kana = 'ｱｱｱｱ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana is invalid")
+    end
+
+    it 'last_name_kanaは数字では登録できない' do
+      @user.last_name_kana = '１１１'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("Last name kana is invalid")
+    end
+
+
+    it 'first_name_kanaは半角だと登録できない' do
+      @user.first_name_kana = 'ｱｱｱｱ'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana is invalid")
+    end
+
+    it 'first_name_kanaは数字では登録できない' do
+      @user.first_name_kana = '１１１'
+      @user.valid?
+      expect(@user.errors.full_messages).to include("First name kana is invalid")
+    end
+
+
 
 
 end
