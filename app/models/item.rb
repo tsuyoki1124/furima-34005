b18c2b1cd200737fe18f818c892afdab8cb01ba4
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-12 17:03:09 +0900

    delivery fee active hashの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,8 @@
 class Item < ApplicationRecord
   validates :prefecture_id, numericality: { other_than: 0 }
   validates :condition_id, numericality: { other_than: 0 }
+  validates :category_id, numericality: { other_than: 0 }
+  validates :delivery_fee_id, numericality: { other_than: 0 }
 
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
