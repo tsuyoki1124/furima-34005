a37530a9c85a5f99ea4897f12fc5be9cc21c9b3a
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-12 17:15:31 +0900

    estimated shipping date active hashの導入

---
+++ app/models/estimated_shipping_date.rb
@@ -0,0 +1,9 @@
+class Estimated_shipping_date < ActiveHash::Base
+  self.data = [
+      {id: 0, name: '---'}, {id: 1, name: '１〜２日で発送'}, 
+      {id: 2, name: '２〜３日で発送'}, {id: 3, name: '４〜７日で発送'}
+  ]
+  include ActiveHash::Associations
+  has_many :items
+
+end
--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,13 @@ class Item < ApplicationRecord
   validates :condition_id, numericality: { other_than: 0 }
   validates :category_id, numericality: { other_than: 0 }
   validates :delivery_fee_id, numericality: { other_than: 0 }
+  validates :estimated_shipping_date_id, numericality: { other_than: 0 }
 
   belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :condition
+  belongs_to :delivery_fee
+  belongs_to :estimated_shipping_date
 
 end
