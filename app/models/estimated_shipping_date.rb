531a4f92b2dc82802dd7e17ca179b3bd457b020c
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-12 19:11:18 +0900

    空で出品した時の遷移の実装

--- app/models/estimated_shipping_date.rb
+++ app/models/estimated_shipping_date.rb
@@ -1,4 +1,4 @@
-class Estimated_shipping_date < ActiveHash::Base
+class EstimatedShippingDate < ActiveHash::Base
   self.data = [
       {id: 0, name: '---'}, {id: 1, name: '１〜２日で発送'}, 
       {id: 2, name: '２〜３日で発送'}, {id: 3, name: '４〜７日で発送'}
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,11 +5,14 @@ class Item < ApplicationRecord
   validates :delivery_fee_id, numericality: { other_than: 0 }
   validates :estimated_shipping_date_id, numericality: { other_than: 0 }
 
-  belongs_to :user
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :prefecture
   belongs_to :condition
+  belongs_to :category
   belongs_to :delivery_fee
   belongs_to :estimated_shipping_date
+
+  belongs_to :user
+  has_one_attached :image
 
 end
