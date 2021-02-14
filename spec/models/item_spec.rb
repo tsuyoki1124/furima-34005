9337f9bed9071faee7bf5f9918e5440641130798
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-14 16:48:27 +0900

    単体テストコードの記述

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,96 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @item = FactoryBot.build(:item)
+      @item.image = fixture_file_upload('app/assets/images/item-sample.png')
+    end
+
+    context '出品できること' do
+      it '全ての値が正常に入力されていれば、出品できること' do
+        expect(@item).to be_valid
+      end
+    end
+
+    context '出品できないこと' do
+      it 'titleが空だと保存できないこと' do
+        @item.title =''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Title can't be blank")
+      end
+
+      it 'textが空だと保存できないこと' do
+        @item.text =''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Text can't be blank")
+      end
+
+      it 'priceが空だと保存できないこと' do
+        @item.price =''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it 'prefecture_idが空だと保存できないこと' do
+        @item.prefecture_id =''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture is not a number")
+      end
+
+      it 'condition_idが空だと保存できないこと' do
+        @item.condition_id =''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition is not a number")
+      end
+
+      it 'category_idが空だと保存できないこと' do
+        @item.category_id =''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category is not a number")
+      end
+
+      it 'delivery_fee_idが空だと保存できないこと' do
+        @item.delivery_fee_id =''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery fee is not a number")
+      end
+
+      it 'estimated_shipping_date_idが空だと保存できないこと' do
+        @item.estimated_shipping_date_id =''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Estimated shipping date is not a number")
+      end
+
+      it 'imageが空だと保存できないこと' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+
+      it 'priceが300円未満だと保存できないこと' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than 300")
+      end
+
+      it 'priceが10000000以上だと保存できないこと' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 9999999")
+      end
+
+      it 'priceが全角数字だと保存できないこと' do
+        @item.price = '１００００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is not a number")
+      end
+
+      it 'userが紐づいていないと保存できないこと' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
