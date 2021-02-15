6c0ccb8b91f8814f8fc086fef567f20d5e45e706
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-15 23:30:37 +0900

    アドレス手入力での遷移防止（indexへ）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-
+  before_action :move_to_index, except: [:index, :show]
+  
   def index
     @items = Item.all.order(created_at: :desc)
   end
@@ -40,5 +41,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :title, :price, :text, :prefecture_id, :condition_id, :category_id, :delivery_fee_id,
                                  :estimated_shipping_date_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user.id
+      redirect_to action: :index
+    end
   end
 end
