a145bb30972d84e2c60f0ef2f66782f6d3bf1b59
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-16 11:48:53 +0900

    destroyの実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_index, except: [:index, :show, :new]
+  before_action :move_to_index, except: [:index, :show, :new, :create]
   
   def index
     @items = Item.all.order(created_at: :desc)
@@ -32,6 +32,12 @@ class ItemsController < ApplicationController
       else
          render :edit
       end
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
   end
 
   private
