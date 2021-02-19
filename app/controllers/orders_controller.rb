81867e14f32fb6c57284bd63074db26731cf36e2
tsuyoki1124 <tsuyoki.tanokura@gmail.com>
2021-02-19 19:23:35 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -41,6 +41,6 @@ class OrdersController < ApplicationController
   end
 
   def move_to_index
-    redirect_to root_path if @item.order.present?
+    redirect_to root_path if @item.order.present? || current_user.id == @item.user.id
   end
 end
